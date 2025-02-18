e9e33c7890e69b27ec8695e4cf9cad05b3821356
Vajay <isaradhi24@gmail.com>
2025-02-18 00:19:41 -0500

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-053a45fff0a704a47"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    key_name = "Terraforam-key"  
+    tags = {
+        Name = "Terraform-Instance"
+    }
 }
